e27ddceab1025d0eda097822ac0d273c68261cfc
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2022-10-25 01:02:19 +0300

    Deploy token

--- first_dApp/hardhat.config.js
+++ first_dApp/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "PnZENMlfUMYkAF0vMAI3j-qDAArcH8IP";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "f6d64e5607949b505f9f258c8e513f84e8a7a0f136a5bf62545457e8de78c9ee";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
